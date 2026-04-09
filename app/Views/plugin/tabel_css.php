82b9498491b5d203af98f701a5fc247211252456
Mahmud <support@cirebonweb.com>
2026-04-09 13:18:45 +0700

    add: require plugin

---
+++ app/Views/plugin/tabel_css.php
@@ -0,0 +1,2 @@
+<link rel="stylesheet" href="<?= base_url('plugin/datatables/datatables.min.css') ?>">
+<link rel="stylesheet" href="<?= base_url('vendor/css/custom_tabel.min.css') ?>">
---
+++ app/Views/plugin/validasi_js.php
@@ -0,0 +1,3 @@
+<script src="<?= base_url('plugin/jquery/jquery.validate.min.js') ?>" defer></script>
+<script src="<?= base_url('plugin/jquery/jquery.validate_id.min.js') ?>" defer></script>
+<script src="<?= base_url('plugin/jquery/additional-methods.min.js') ?>" defer></script>
