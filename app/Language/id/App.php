dba8cac72536dc9ef1d4cfe91fe21bdcb70cba14
Mahmud <support@cirebonweb.com>
2026-04-13 15:51:29 +0700

    add: custom bahasa

---
+++ app/Language/id/App.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+    'new' => 'Baru',
+    'add'  => 'Tambah',
+    'edit'  => 'Edit',
+    'update' => 'Update',
+    'delete' => 'Hapus',
+    'copy' => 'Salin',
+    'save'  => 'Simpan',
+    'confirm' => 'Konfirmasi',
+    'cancel'  => 'Batal',
+    'remove-title' => 'Apkah anda yakin ingin menghapus data?',
+    'remove-reset' => 'Apkah anda yakin ingin menghapus semua data?',
+    'remove-text' => 'Data akan dihapus secara permanen!',
+    'insert-success' => 'Data berhasil disimpan.',
+    'insert-error' => 'Data gagal disimpan!',
+    'update-success' => 'Data berhasil diperbaharui.',
+    'update-error' => 'Data gagal diperbaharui!',
+    'delete-success' => 'Data berhasil dihapus.',
+    'delete-error' => 'Data gagal dihapus!',
+    'invalid-data' => 'Data yang diterima server tidak valid!'
+ ];
